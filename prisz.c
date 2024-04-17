606a210959af723cc9cada07a7c73ff1289756de
Daniel Rodrigues <danielprodrigues0@gmail.com>
2024-04-16 21:03:03 -0300

    changes

--- prisz.c
+++ prisz.c
@@ -1,21 +1,101 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode(void) {
-  struct termios raw;
+/*** defines ***/
 
-  tcgetattr(STDIN_FILENO, &raw);
+#define CTRL_KEY(k) ((k) & 0x1f)
 
-  raw.c_lflag &= ~(ECHO);
+/*** data ***/
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  perror(s);
+  exit(1);
 }
+
+void disabledRawMode(void) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+    die("tcsetattr");
+  }
+}
+
+void enableRawMode(void) {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+    die("tcgetattr");
+  }
+
+  atexit(disabledRawMode);
+
+  struct termios raw = orig_termios;
+
+  raw.c_iflag &= ~(BRKINT | IXON | ICRNL | INPCK | ISTRIP);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+    die("tcsetattr");
+  }
+}
+
+char editorReadKey(void) {
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) {
+      die("read");
+    }
+  }
+
+  return c;
+}
+
+/*** output ***/
+
+void editorRefreshScreen(void) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+
+void editorProcessKeypress(void) {
+  char c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
+}
+
+/*** init ***/
 
 int main(void) {
   enableRawMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (1) {
+    editorRefreshScreen();
+    editorProcessKeypress();
+  }
+
 	return 0;
 }
 
